5b991a5d9f635dda25efa1f510f6d1421b5b86fd
= <thangokmn1209@gmail.com>
2022-01-27 01:49:08 +0700

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,105 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <Nav :nav="nav" @onNav="changeNav" />
+    <div v-if="nav === 1">
+      <button
+        class="btn btn-dark"
+        style="float: right; margin: -50px 50px 0 0"
+        @click="onToggleModal"
+      >
+        Add Card
+      </button>
+      <div class="card-list">
+        <Card
+          v-for="card in cards"
+          :key="card.id"
+          class="card-item"
+          :id="card.id"
+          @deleteCard="deleteCard"
+        >
+          <template v-slot:name>
+            {{ card.name }}
+          </template>
+          <template v-slot:title>
+            {{ card.title }}
+          </template>
+          <template v-slot:description>
+            {{ card.description }}
+          </template>
+        </Card>
+      </div>
+      <Modal
+        @closeModal="onToggleModal"
+        @addCart="addCart"
+        v-if="isShowModal"
+      ></Modal>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Nav from "./components/Nav.vue";
+import Card from "./components/Card.vue";
+import Modal from "./components/Modal.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    Nav,
+    Card,
+    Modal,
+  },
+  data() {
+    return {
+      isShowModal: false,
+      cards: [
+        {
+          id: 1,
+          name: "Card 1",
+          title: "Title 1",
+          description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium possimus ut commodi consectetur ullam sequi fuga voluptatem ratione aliquid iusto!",
+        },
+        {
+          id: 2,
+          name: "Card 2",
+          title: "Title 2",
+          description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium possimus ut commodi consectetur ullam sequi fuga voluptatem ratione aliquid iusto!",
+        },
+        {
+          id: 3,
+          name: "Card 3",
+          title: "Title 3",
+          description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium possimus ut commodi consectetur ullam sequi fuga voluptatem ratione aliquid iusto!",
+        },
+      ],
+      nav: 1,
+    };
+  },
+  methods: {
+    onToggleModal() {
+      this.isShowModal = !this.isShowModal;
+    },
+    addCart(name, title, description) {
+      this.cards = [
+        ...this.cards,
+        {
+          id: this.cards.length + 1,
+          name: name,
+          title: title,
+          description: description,
+        },
+      ];
+    },
+    deleteCard(id) {
+      this.cards = this.cards.filter((card) => card.id !== id);
+    },
+    changeNav(id) {
+      this.nav = id;
+      console.log(this.nav);
+    },
   },
 };
 </script>
@@ -21,6 +111,21 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.card {
+  &-list {
+    margin: 70px 50px 50px;
+    display: flex;
+    flex-wrap: wrap;
+  }
+  &-item {
+    margin-bottom: 15px;
+    margin-right: 15px;
+    max-width: calc(25% - 11.25px);
+    &:nth-child(4n) {
+      margin-right: 0;
+    }
+  }
 }
 </style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,40 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+      <div
+        class="collapse navbar-collapse justify-content-center"
+        id="navbarNav"
+      >
+        <ul class="navbar-nav">
+          <li @click="onNav(1)" class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li @click="onNav(2)" class="nav-item">
+            <a class="nav-link" href="#">Cart</a>
+          </li>
+          <li @click="onNav(3)" class="nav-item">
+            <a class="nav-link" href="#">Pricing</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Nav",
+  methods: {
+    onNav(id) {
+      this.$emit("onNav", id);
+    },
+  },
+  props: {
+    nav: {
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss"></style>
